f7d910e90af699f5d77b713a8e7dc24843535f98
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-01-09 01:15:33 +0700

    commit

--- modules/article/models/ArticleViews.php
+++ modules/article/models/ArticleViews.php
@@ -360,10 +360,10 @@ class ArticleViews extends CActiveRecord
 	 */
 	protected function beforeValidate() {
 		if(parent::beforeValidate()) {		
-			if($this->isNewRecord) {
+			if($this->isNewRecord)
 				$this->user_id = !Yii::app()->user->isGuest ? Yii::app()->user->id : 0;
-				$this->views_ip = $_SERVER['REMOTE_ADDR'];
-			}		
+			
+			$this->views_ip = $_SERVER['REMOTE_ADDR'];
 		}
 		return true;
 	}
