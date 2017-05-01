4e0a81af37d75381b0130f75a8911ff26bac6f00
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-05-01 21:55:26 +0700

    rename contact information

--- modules/article/ArticleModule.php
+++ modules/article/ArticleModule.php
@@ -28,8 +28,10 @@ class ArticleModule extends CWebModule
 		return $this->_assetsUrl;
 	}
 
-	public function beforeControllerAction($controller, $action) {
-		if(parent::beforeControllerAction($controller, $action)) {
+	public function beforeControllerAction($controller, $action) 
+	{
+		if(parent::beforeControllerAction($controller, $action)) 
+		{
 			// this method is called before any module controller action is performed
 			// you may place customized code here
 			//list public controller in this module
--- modules/article/controllers/api/SiteController.php
+++ modules/article/controllers/api/SiteController.php
@@ -20,7 +20,7 @@
  * @copyright Copyright (c) 2016 Ommu Platform (opensource.ommu.co)
  * @created date 23 Juni 2016, 14:46 WIB
  * @link https://github.com/ommu/Articles
- * @contect (+62)856-299-4114
+ * @contact (+62)856-299-4114
  *
  *----------------------------------------------------------------------------------------------------------
  */
