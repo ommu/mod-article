70c0c863a27c957ce93960c64c3b004863554d0c
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-16 10:50:50 +0700

    modify dateFormat with locale

--- views/history/like/admin_view.php
+++ views/history/like/admin_view.php
@@ -45,7 +45,7 @@
 			),
 			array(
 				'name'=>'likes_date',
-				'value'=>!in_array($model->likes_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->likes_date, true) : '-',
+				'value'=>!in_array($model->likes_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->likes_date) : '-',
 			),
 			array(
 				'name'=>'likes_ip',
--- views/history/view/admin_view.php
+++ views/history/view/admin_view.php
@@ -41,7 +41,7 @@
 			),
 			array(
 				'name'=>'view_date',
-				'value'=>!in_array($model->view_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->view_date, true) : '-',
+				'value'=>!in_array($model->view_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->view_date) : '-',
 			),
 			array(
 				'name'=>'view_ip',
