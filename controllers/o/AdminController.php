55af6e904cecd70161121e2aefb47a67d654ef78
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-22 00:49:58 +0700

    change expression in accessRules

--- controllers/o/AdminController.php
+++ controllers/o/AdminController.php
@@ -12,7 +12,7 @@
  *	Add
  *	Edit
  *	View
- *	RunAction
+ *	Runaction
  *	Delete
  *	Publish
  *	Headline
@@ -77,7 +77,7 @@ class AdminController extends Controller
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('index','manage','add','edit','view','runaction','delete','publish','headline','getcover','insertcover'),
 				'users'=>array('@'),
-				'expression'=>'in_array($user->level, array(1,2))',
+				'expression'=>'in_array(Yii::app()->user->level, array(1,2))',
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
@@ -224,7 +224,7 @@ class AdminController extends Controller
 	 * Displays a particular model.
 	 * @param integer $id the ID of the model to be displayed
 	 */
-	public function actionRunAction() {
+	public function actionRunaction() {
 		$id       = $_POST['trash_id'];
 		$criteria = null;
 		$actions  = Yii::app()->getRequest()->getParam('action');
--- controllers/o/MediaController.php
+++ controllers/o/MediaController.php
@@ -11,7 +11,7 @@
  *	Manage
  *	Edit
  *	View
- *	RunAction
+ *	Runaction
  *	Delete
  *	Publish
  *	Setcover
@@ -74,7 +74,7 @@ class MediaController extends Controller
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('index','manage','edit','view','runaction','delete','publish','setcover'),
 				'users'=>array('@'),
-				'expression'=>'in_array($user->level, array(1,2))',
+				'expression'=>'in_array(Yii::app()->user->level, array(1,2))',
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
@@ -175,7 +175,7 @@ class MediaController extends Controller
 	 * Displays a particular model.
 	 * @param integer $id the ID of the model to be displayed
 	 */
-	public function actionRunAction() {
+	public function actionRunaction() {
 		$id       = $_POST['trash_id'];
 		$criteria = null;
 		$actions  = Yii::app()->getRequest()->getParam('action');
