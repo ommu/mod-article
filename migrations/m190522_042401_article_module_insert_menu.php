43d6e956d40eeab9330ec637dd0cfac635ed21b4
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-05-22 04:56:55 +0700

    add migrate script (permission + manu)

---
+++ migrations/m190522_042401_article_module_insert_menu.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * m190522_042401_article_module_insert_menu
+ * 
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2019 OMMU (www.ommu.co)
+ * @created date 22 May 2019, 04:25 WIB
+ * @link https://github.com/ommu/mod-article
+ *
+ */
+
+use Yii;
+use app\models\Menu;
+
+class m190522_042401_article_module_insert_menu extends \yii\db\Migration
+{
+	public function up()
+	{
+		$tableName = Yii::$app->db->tablePrefix . 'ommu_core_menus';
+		if(Yii::$app->db->getTableSchema($tableName, true)) {
+			$this->batchInsert('ommu_core_menus', ['name', 'module', 'icon', 'parent', 'route', 'order', 'data'], [
+				['Articles', 'article', null, Menu::getParentId('Publications#rbac'), '/article/admin/index', null, null],
+				['Article Settings', 'article', null, Menu::getParentId('Settings#rbac'), '/article/setting/admin/index', null, null],
+			]);
+		}
+	}
+}
---
+++ migrations/m190522_042401_article_module_insert_role.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * m190522_042401_article_module_insert_role
+ * 
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2019 OMMU (www.ommu.co)
+ * @created date 22 May 2019, 04:25 WIB
+ * @link https://github.com/ommu/mod-article
+ *
+ */
+
+use Yii;
+
+class m190522_042401_article_module_insert_role extends \yii\db\Migration
+{
+	public function up()
+	{
+		$tableName = Yii::$app->db->tablePrefix . 'ommu_core_auth_item';
+		if(Yii::$app->db->getTableSchema($tableName, true)) {
+			$this->batchInsert('ommu_core_auth_item', ['name', 'type', 'data', 'created_at'], [
+				['articleModLevelAdmin', '2', '', time()],
+				['articleModLevelModerator', '2', '', time()],
+				['/article/admin/*', '2', '', time()],
+				['/article/admin/index', '2', '', time()],
+				['/article/o/image/*', '2', '', time()],
+				['/article/o/image/index', '2', '', time()],
+				['/article/o/file/*', '2', '', time()],
+				['/article/o/file/index', '2', '', time()],
+				['/article/o/view/*', '2', '', time()],
+				['/article/o/view/index', '2', '', time()],
+				['/article/o/download/*', '2', '', time()],
+				['/article/o/download/index', '2', '', time()],
+				['/article/o/tag/*', '2', '', time()],
+				['/article/o/tag/index', '2', '', time()],
+				['/article/o/like/*', '2', '', time()],
+				['/article/o/like/index', '2', '', time()],
+				['/article/history/download/*', '2', '', time()],
+				['/article/history/view/*', '2', '', time()],
+				['/article/history/like/*', '2', '', time()],
+				['/article/setting/admin/index', '2', '', time()],
+				['/article/setting/admin/update', '2', '', time()],
+				['/article/setting/admin/delete', '2', '', time()],
+				['/article/setting/category/*', '2', '', time()],
+			]);
+		}
+
+		$tableName = Yii::$app->db->tablePrefix . 'ommu_core_auth_item_child';
+		if(Yii::$app->db->getTableSchema($tableName, true)) {
+			$this->batchInsert('ommu_core_auth_item_child', ['parent', 'child'], [
+				['userAdmin', 'articleModLevelAdmin'],
+				['userModerator', 'articleModLevelModerator'],
+				['articleModLevelAdmin', 'articleModLevelModerator'],
+				['articleModLevelAdmin', '/article/setting/admin/update'],
+				['articleModLevelAdmin', '/article/setting/admin/delete'],
+				['articleModLevelAdmin', '/article/setting/category/*'],
+				['articleModLevelModerator', '/article/setting/admin/index'],
+				['articleModLevelModerator', '/article/admin/*'],
+				['articleModLevelModerator', '/article/o/image/*'],
+				['articleModLevelModerator', '/article/o/file/*'],
+				['articleModLevelModerator', '/article/o/view/*'],
+				['articleModLevelModerator', '/article/o/download/*'],
+				['articleModLevelModerator', '/article/o/tag/*'],
+				['articleModLevelModerator', '/article/o/like/*'],
+				['articleModLevelModerator', '/article/history/download/*'],
+				['articleModLevelModerator', '/article/history/view/*'],
+				['articleModLevelModerator', '/article/history/like/*'],
+			]);
+		}
+	}
+}
