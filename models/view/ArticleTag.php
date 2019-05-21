e927c40ace6c555541d6557e7c057e2ccdf44785
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-05-21 12:29:43 +0700

    commit

--- models/view/ArticleTag.php
+++ models/view/ArticleTag.php
@@ -1,26 +1,26 @@
 <?php
 /**
  * ArticleTag
- *
- * This is the model class for table "_article_tag".
- *
- * The followings are the available columns in table "_article_tag":
- * @property string $tag_id
- * @property string $articles
- * @property string $article_all
-
+ * 
  * @author Putra Sudaryanto <putra@sudaryanto.id>
  * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2017 OMMU (www.ommu.co)
  * @created date 20 October 2017, 10:34 WIB
+ * @modified date 21 May 2019, 12:25 WIB
  * @link https://github.com/ommu/mod-article
+ *
+ * This is the model class for table "_article_tag".
+ *
+ * The followings are the available columns in table "_article_tag":
+ * @property integer $tag_id
+ * @property string $articles
+ * @property integer $article_all
  *
  */
 
 namespace ommu\article\models\view;
 
 use Yii;
-use yii\helpers\Url;
 
 class ArticleTag extends \app\components\ActiveRecord
 {
@@ -35,6 +35,14 @@ class ArticleTag extends \app\components\ActiveRecord
 	}
 
 	/**
+	 * @return string the primarykey column
+	 */
+	public static function primaryKey()
+	{
+		return ['tag_id'];
+	}
+
+	/**
 	 * @return array validation rules for model attributes.
 	 */
 	public function rules()
@@ -44,10 +52,6 @@ class ArticleTag extends \app\components\ActiveRecord
 			[['tag_id', 'article_all'], 'integer'],
 			[['articles'], 'number'],
 		];
-	}
-
-	public static function primaryKey() {
-		return ['tag_id'];
 	}
 
 	/**
@@ -61,7 +65,7 @@ class ArticleTag extends \app\components\ActiveRecord
 			'article_all' => Yii::t('app', 'Article All'),
 		];
 	}
-	
+
 	/**
 	 * Set default columns to display
 	 */
@@ -74,69 +78,41 @@ class ArticleTag extends \app\components\ActiveRecord
 			'class' => 'yii\grid\SerialColumn',
 			'contentOptions' => ['class'=>'center'],
 		];
-		$this->templateColumns['tag_id'] = 'tag_id';
-		$this->templateColumns['articles'] = 'articles';
-		$this->templateColumns['article_all'] = 'article_all';
+		$this->templateColumns['tag_id'] = [
+			'attribute' => 'tag_id',
+			'value' => function($model, $key, $index, $column) {
+				return $model->tag_id;
+			},
+		];
+		$this->templateColumns['articles'] = [
+			'attribute' => 'articles',
+			'value' => function($model, $key, $index, $column) {
+				return $model->articles;
+			},
+		];
+		$this->templateColumns['article_all'] = [
+			'attribute' => 'article_all',
+			'value' => function($model, $key, $index, $column) {
+				return $model->article_all;
+			},
+		];
 	}
 
 	/**
-	 * before validate attributes
+	 * User get information
 	 */
-	public function beforeValidate()
+	public static function getInfo($id, $column=null)
 	{
-		if(parent::beforeValidate()) {
+		if($column != null) {
+			$model = self::find()
+				->select([$column])
+				->where(['tag_id' => $id])
+				->one();
+			return $model->$column;
+			
+		} else {
+			$model = self::findOne($id);
+			return $model;
 		}
-		return true;
-	}
-
-	/**
-	 * before save attributes
-	 */
-	public function beforeSave($insert)
-	{
-		if(parent::beforeSave($insert)) {
-			// Create action
-		}
-		return true;	
-	}
-
-	/**
-	 * after validate attributes
-	 */
-	public function afterValidate()
-	{
-		parent::afterValidate();
-		// Create action
-		
-		return true;
-	}
-	
-	/**
-	 * After save attributes
-	 */
-	public function afterSave($insert, $changedAttributes)
-	{
-		parent::afterSave($insert, $changedAttributes);
-		// Create action
-	}
-
-	/**
-	 * Before delete attributes
-	 */
-	public function beforeDelete()
-	{
-		if(parent::beforeDelete()) {
-			// Create action
-		}
-		return true;
-	}
-
-	/**
-	 * After delete attributes
-	 */
-	public function afterDelete()
-	{
-		parent::afterDelete();
-		// Create action
 	}
 }
